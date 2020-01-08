a07db14b5f64bbafee0b07505be7c23edf0ffd40
Carlos Alberto <carlos.a.alberto94@gmail.com>
2020-01-08 20:19:44 -0300

    Changes in login and register  JS file

--- src/js/login.js
+++ src/js/login.js
@@ -2,15 +2,12 @@
 var form = document.querySelector(".login__container--input");  
 form.addEventListener('submit', validateUser);
 
-
-
 function validateUser(e) { 
     e.preventDefault()
-    var userList = localStorage.getItem("users"); //Recupero la lista de usuarios
+    var userList = localStorage.getItem("users"); 
     var userArr = JSON.parse(userList)
     let user = document.querySelector(".input.user").value
     let pass = document.querySelector(".input.pass").value
-    
     userArr.forEach((element) => {
         if((element.user === user) && (element.password === pass)){
             window.location.href="./src/pages/main.html"
--- src/js/register.js
+++ src/js/register.js
@@ -1,36 +1,31 @@
 
 var form = document.querySelector("form");
-var userList = localStorage.getItem("users")
-var arrList 
+var userList = localStorage.getItem("users");
+var arrList; 
 form.addEventListener('submit', (e)=> {e.preventDefault(); validateFields()})
 
-    
 
-function validateFields(){
+function validateFields() {
     var user = document.querySelector(".input.user").value;
     var email = document.querySelector(".input.email").value;
     var password = document.querySelector("input.pass").value;
-    if ((user != null || "" || undefined ) && (email != null || "") && (password != null || "")) {
-        let newUs ={"user":user,"email":email,"password":password}
-        registerUser(newUs)
+    if ((user != null || "" ) && (email != null || "") && (password != null || "")) {
+        let newUs = {"user":user,"email":email,"password":password};
+        registerUser(newUs);
     }
 }
 
-function registerUser(newUs) {
 
-    
+function registerUser(newUs) {
     if (userList == null) {
-        arrList = []
-        arrList.push(newUs)
-        console.log(arrList)
-    }else {
-        arrList = JSON.parse(userList)
-        arrList.push(newUs)
-        console.log(arrList)
+        arrList = [];
+        arrList.push(newUs);
+    } else {
+        arrList = JSON.parse(userList);
+        arrList.push(newUs);
     } 
        
-    localStorage.setItem("users", JSON.stringify(arrList))
-    
-    //window.location.href='/index.html'  
+    localStorage.setItem("users", JSON.stringify(arrList));
+    window.location.href='/index.html';  
    
 }
